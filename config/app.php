b830a826fce7f8b9afaacad5059a1d9b07bf25f8
gibransebagaisaturajp <gibransebagai2@gmail.com>
2019-08-03 08:49:59 +0700

    hosting terbaru

--- config/app.php
+++ config/app.php
@@ -161,7 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        //add on
+        Laratrust\LaratrustServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -174,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Laratrust\LaratrustServiceProvider::class,
     ],
 
     /*
@@ -225,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        //add on
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
